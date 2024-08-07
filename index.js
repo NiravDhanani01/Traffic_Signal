a0bfcba2605b0ce50ed71580ac599e3af1d37084
NiravDhanani <nirav.devotee@gmail.com>
2024-08-07 18:17:26 +0530

    red light waiting time continue

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const startTrafficLight = async () => {
   let response = await timeData.json();
 
   // totalTime = parseInt(document.getElementById("userInput").value);
-  let totalTime = 8;
+  totalTime = 10;
   // if (isNaN(totalTime) || totalTime < 100) {
   //   alert("Please enter a valid number greater than 100");
   //   return;
@@ -36,8 +36,8 @@ const startTrafficLight = async () => {
 
   firstLine = Math.floor((totalTime * line1) / 100);
   secondLine = Math.floor((totalTime * line2) / 100);
-  thirdLine = Math.floor((totalTime * line3) / 100);
-  forthLine = Math.floor((totalTime * line4) / 100);
+  thirdLine = Math.ceil((totalTime * line3) / 100);
+  forthLine = Math.ceil((totalTime * line4) / 100);
 
   let currentDate = new Date();
   let hours = currentDate.getHours().toString().padStart(2, 0);
@@ -45,7 +45,7 @@ const startTrafficLight = async () => {
 
   let realTime = `${hours}${minutes}`;
 
-  response.map((item) => {
+  let jsonTime = response.map((item) => {
     let separateStartTime = item.startTime.split("/");
     let separateEndTime = item.endTime.split("/");
 
@@ -56,17 +56,21 @@ const startTrafficLight = async () => {
 
     let startTime = `${startHours}${startMinute}`;
     let endTime = `${separateEndHour}${separateEndMinute}`;
-
-    if (
-      Number(realTime) >= Number(startTime) &&
-      Number(realTime) <= Number(endTime)
-    ) {
-      checkYellowLight();
-    } else {
-      goGreen(firstLine, secondLine, thirdLine, forthLine);
-    }
+    return { startTime, endTime };
   });
 
+  let check = jsonTime.find((item) => {
+    return item.startTime <= realTime && item.endTime >= realTime;
+  });
+
+  if (check) {
+    checkYellowLight();
+  } else {
+    goGreen(firstLine, secondLine, thirdLine, forthLine);
+  }
+
+  document.querySelector(".startTime").setAttribute("disabled", "disabled");
+  document.querySelector(".stopBtn").removeAttribute("disabled", "disabled");
   document.getElementById("userInput").value = "";
   document.getElementById("1st").value = " ";
   document.getElementById("2nd").value = " ";
@@ -77,6 +81,7 @@ const startTrafficLight = async () => {
 // set timer according to ratio, other wise all equal
 const goGreen = (firstLine, secondLine, thirdLine, forthLine) => {
   let time;
+  let timer1 = 10;
 
   function update() {
     if (currentPosition == 0) {
@@ -91,34 +96,39 @@ const goGreen = (firstLine, secondLine, thirdLine, forthLine) => {
     if (currentPosition == 3) {
       time = forthLine;
     }
+    if (currentPosition == 4) {
+      currentPosition = 0;
+    }
 
     // set light according to time
     green.forEach((light) => {
       light.style.backgroundColor = "transparent";
       light.style.boxShadow = "none";
     });
-    red.forEach((light) => {
-      light.style.backgroundColor = "rgb(255, 0, 0)";
-      light.style.boxShadow = "0px 0px 40px rgb(255, 0, 0)";
+
+    //set light for red
+    red.forEach((light, index) => {
+      if (index != currentPosition) {
+        light.style.backgroundColor = "rgb(255, 0, 0)";
+        light.style.boxShadow = "0px 0px 40px rgb(255, 0, 0)";
+        light.style.transition = "all 1s";
+      } else {
+        red[currentPosition].style.backgroundColor = "transparent";
+        red[currentPosition].style.boxShadow = "none";
+      }
     });
-    timer.forEach((time) => time);
 
     // at the position light setting
     green[currentPosition].style.backgroundColor = "rgb(0, 255, 0)";
     green[currentPosition].style.boxShadow = "0px 0px 40px rgb(0, 255, 0)";
-    red[currentPosition].style.backgroundColor = "transparent";
-    red[currentPosition].style.boxShadow = "none";
+    green[currentPosition].style.transition = "all 1s";
 
     let decrementTime = time;
-
-    if (currentPosition === 4) {
-      currentPosition = 0;
-    }
-
     countdown = setInterval(() => {
       timer[currentPosition].innerText = `${decrementTime} s`;
       timer[currentPosition].style.color = "green";
-      redTimerUpdate(
+
+      redLightTimer(
         currentPosition,
         firstLine,
         secondLine,
@@ -126,6 +136,7 @@ const goGreen = (firstLine, secondLine, thirdLine, forthLine) => {
         forthLine
       );
       decrementTime--;
+
       if (decrementTime < 0) {
         currentPosition++;
         clearInterval(countdown);
@@ -136,50 +147,94 @@ const goGreen = (firstLine, secondLine, thirdLine, forthLine) => {
   update();
 };
 
-const redTimerUpdate = (
+const redLightTimer = (
   currentPosition,
   firstLine,
   secondLine,
   thirdLine,
   forthLine
 ) => {
-  let waitingTime;
+  let test = totalTime - firstLine;
+  console.log(totalTime, firstLine);
 
   timer.forEach((time, index) => {
     if (index !== currentPosition) {
       time.style.color = "red";
     }
-    if (currentPosition == 1) {
-      waitingTime = firstLine;
-    }
-    if (currentPosition == 2) {
-      waitingTime = firstLine * 2;
-    }
-    if (currentPosition == 3) {
-      waitingTime = firstLine * 3;
-    }
-    if (currentPosition == 4) {
-      currentPosition = 0;
-      waitingTime = firstLine * 4;
-    }
-
-    console.log(waitingTime);
-
-    let remainingTIme = setInterval(() => {
-      time[currentPosition + 1].innerText = waitingTime;
-      waitingTime--;
-
-      if (waitingTime < 1) {
-        clearInterval(remainingTIme);
-      }
-    }, 1000);
   });
+
+  let a = setInterval(() => {
+    timer[3].innerHTML = `${test}`;
+    console.log(test);
+    test--;
+    if (test < 1) {
+      clearInterval(a);
+    }
+  }, 1000);
+
+  let b = setInterval(() => {
+    timer[2].innerHTML = `${test}`;
+    console.log(test);
+    test--;
+    if (test < 1) {
+      clearInterval(b);
+    }
+  }, 1000);
+
+  //   let time3 = totalTime;
+  //   let time0 = firstLine;
+  //   let time1 = secondLine * 2;
+  //   let time2 = thirdLine;
+
+  //   let a = setInterval(() => {
+  //     timer[1].innerText = `${time0}`;
+  //     time0--;
+  //     if (time0 < 0) {
+  //       clearInterval(a);
+  //     }
+  //   }, 1000);
+
+  //   let b = setInterval(() => {
+  //     timer[2].innerText = `${time1}`;
+  //     time1--;
+  //     if (time1 < 0) {
+  //       clearInterval(b);
+  //     }
+  //   }, 1000);
+
+  //   let c = setInterval(() => {
+  //     timer[3].innerText = `${time2}`;
+  //     time2--;
+  //     if (time2 < 0) {
+  //       clearInterval(c);
+  //     }
+  //   });
+  //   let d = setTimeout(() => {
+  //     let dd = setInterval(() => {
+  //       timer[0].innerText = time3;
+  //       time3--;
+  //       if (time3 < 0) {
+  //         clearInterval(dd);
+  //       }
+  //     }, 1000);
+  //   }, 2000);
 };
 
 const checkYellowLight = () => {
   yellow.forEach((item) => {
-    item.style.backgroundColor = "yellow";
-    item.style.boxShadow = " 0px 0px 40px yellow";
+    let blinking = setInterval(() => {
+      item.style.backgroundColor = "yellow";
+      item.style.boxShadow = " 0px 0px 40px yellow";
+      item.style.transition = "all 1s";
+    }, 1000);
+    let stopBlinking = setInterval(() => {
+      item.style.boxShadow = "";
+      item.style.backgroundColor = "";
+    }, 2000);
   });
   timer.forEach((time) => (time.style.color = "yellow"));
+};
+
+const stop = () => {
+  location.reload();
 };
